b748aed429bdc8ed272f8d21d54162e92d30b6de
Rômulo Magno <romulo.magno@opah.com.br>
2021-07-17 10:57:29 -0300

    limpeza diretorio

--- 1a-arquivos-aula-app2-jokenpo-criando-interface/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:jokenpo/Jogo.dart';
-
-void main(){
-  runApp(MaterialApp(
-    home: Jogo(),
-    debugShowCheckedModeBanner: false,
-  ));
-}
--- 1a-arquivos-aula-app3-alcool-gasolina-calculando-resultado/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:alcool_gasolina/Home.dart';
-import 'package:flutter/material.dart';
-
-void main(){
-  runApp(MaterialApp(
-    home: Home(),
-  ));
-}
