e8bfd3ce8be77d74bf49d51f7a83a7b8fa336078
rvmagno <rvmagno@gmail.com>
2021-08-19 12:31:30 -0300

    salvando e atualizando anotacoes.

--- minhas_anotacoes/lib/Home.dart
+++ minhas_anotacoes/lib/Home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:minhas_anotacoes/helper/AnotacaoHelper.dart';
+import 'package:minhas_anotacoes/model/Anotacao.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,15 +15,28 @@ class _HomeState extends State<Home> {
 
   TextEditingController _tituloController = TextEditingController();
   TextEditingController _descricaoController = TextEditingController();
+  var _db = AnotacaoHelper();
+  List<Anotacao> _anotacoes = <Anotacao>[];
 
-  _exibirTelaCadastro(){
+  _exibirTelaCadastro({Anotacao? anotacao}){
 
-    print ("");
+    String _txtAcao = "Salvar";
+    _tituloController.text = "" ;
+    _descricaoController.text = "" ;
+
+    if(anotacao != null){
+      _txtAcao = "Atualizar";
+      _tituloController.text = anotacao.titulo! ;
+      _descricaoController.text = anotacao.descricao! ;
+    }
+
+
+
     showDialog(
         context: context,
         builder: (context){
           return AlertDialog(
-            title: Text("Adicionar anotacao"),
+            title: Text("$_txtAcao anotacao"),
             content: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
@@ -47,23 +64,127 @@ class _HomeState extends State<Home> {
               ),
               FlatButton(
                   onPressed: () {
+                    _salvarAtualizarAnotacao(anotacaoSelecionada: anotacao);
                     Navigator.pop(context);
                   },
-                  child: Text("salvar")
+                  child: Text(_txtAcao)
               )
             ],
           );
         });
   }
 
+  _recuperarAnotacoes() async {
+
+    List recuperadas =  await _db.recuperarAnotacao();
+
+    List<Anotacao>? tempList = <Anotacao>[];
+    for(var item in recuperadas){
+      Anotacao nota = Anotacao.fromMap(item);
+      tempList.add(nota);
+    }
+    setState(() {
+      _anotacoes = tempList!;
+    });
+
+    tempList = null;
+  }
+
+  _salvarAtualizarAnotacao({Anotacao? anotacaoSelecionada}) async {
+    String titulo = _tituloController.text;
+    String descricao = _descricaoController.text;
+
+    // Salvando
+    if(anotacaoSelecionada == null){
+      Anotacao anotacao = Anotacao(titulo, descricao, DateTime.now().toString());
+      var salvarAnotacao = await _db.salvarAnotacao(anotacao);
+    }
+    // Atualizando
+    else{
+      anotacaoSelecionada.titulo = titulo;
+      anotacaoSelecionada.descricao = descricao;
+      anotacaoSelecionada.data = DateTime.now().toString();
+      int resultado = await _db.atualizarAnotacao(anotacaoSelecionada);
+    }
+
+    _tituloController.clear();
+    _descricaoController.clear();
+
+    _recuperarAnotacoes();
+
+  }
+  _formatarData(String data){
+    DateTime dtConvertida = DateTime.parse(data);
+    initializeDateFormatting('pt_BR', null);
+    var formatter = DateFormat("dd/MM/y H:m:s");
+    // var formatter = DateFormat.yMd("pt_BR");
+
+    return formatter.format(dtConvertida);
+    // return data;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _recuperarAnotacoes();
+  }
+
   @override
   Widget build(BuildContext context) {
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text("minhas anotacoes"),
         backgroundColor: Colors.lightBlue,
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _anotacoes.length,
+              itemBuilder: (context, index){
+                final anotacao = _anotacoes[index];
+                return Card(
+                  child: ListTile(
+                    title: Text("(${anotacao.id}) - ${anotacao.titulo!}"),
+                    subtitle: Text("${_formatarData(anotacao.data!)} - ${anotacao.descricao}"),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        GestureDetector(
+                          onTap: (){
+                            _exibirTelaCadastro(anotacao: anotacao);
+                          },
+                        child:
+                          Padding(
+                            padding: EdgeInsets.only(right: 20),
+                            child: Icon(
+                                Icons.edit,
+                                color: Colors.lightGreen,
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: (){
+
+                          },
+                        child:
+                          Padding(
+                            padding: EdgeInsets.only(right: 0),
+                            child: Icon(
+                                Icons.remove,
+                                color: Colors.redAccent,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              }))
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.lightBlueAccent,
         foregroundColor: Colors.white,
--- minhas_anotacoes/lib/helper/AnotacaoHelper.dart
+++ minhas_anotacoes/lib/helper/AnotacaoHelper.dart
@@ -1,29 +1,42 @@
+import 'package:minhas_anotacoes/model/Anotacao.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-
 class AnotacaoHelper {
 
+  static final String tbAnotacao = "anotacao";
   static final AnotacaoHelper _anotacaoHelper = AnotacaoHelper._internal();
-  Database _db ;
+  Database? _db ;
 
   factory AnotacaoHelper(){
     return _anotacaoHelper;
   }
 
-  AnotacaoHelper._internal();
+  AnotacaoHelper._internal(){
+  }
 
   get db async {
-    if(_db != null){
+
+    if( _db != null ){
+      return _db;
+    }else{
+      _db = await inicializarDB();
       return _db;
     }
 
-    _db = await _inicializarDB();
-    return _db;
   }
 
+  _onCreate(Database db, int version) async {
+    String sql = "CREATE TABLE $tbAnotacao ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        " titulo VARCHAR, "
+        " descricao TEXT,"
+        " data DATETIME)";
+    await db.execute(sql);
 
-  _inicializarDB() async{
+  }
+
+  inicializarDB() async{
     final caminhoBandoDados = await getDatabasesPath();
     final localBancoDados = join(caminhoBandoDados, "bd_minahs_anotacoes.db");
 
@@ -36,9 +49,64 @@ class AnotacaoHelper {
     return db;
   }
 
-  _onCreate(Database db, int version) async {
-    String sql = "CREATE TABLE anotacao (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo VARCHAR, descricao TEXT, data DATETIME)";
-    await db.execute(sql);
+  Future<int> salvarAnotacao(Anotacao anotacao) async {
+
+    var bancoDados = await db;
+
+    int resultado = await bancoDados.insert(tbAnotacao, anotacao.toMap());
+    return resultado;
+  }
+
+  recuperarAnotacao() async {
+
+    var bancoDados = await db;
+    String sql = "SELECT * FROM $tbAnotacao ORDER BY data DESC ";
+    List anotacoes = await bancoDados.rawQuery(sql);
+
+/*
+    String gambiarra = "SELECT * FROM $tbAnotacao WHERE id <= 2 ORDER BY data DESC ";
+    List anotacoesGambiarra = await bancoDados.rawQuery(sql);
+
+
+
+    List<Anotacao>? tempList = <Anotacao>[];
+    for(var item in anotacoesGambiarra){
+      Anotacao nota = Anotacao.fromMap(item);
+      tempList.add(nota);
+    }
+
+    for(var nota in tempList){
+      print(nota.data);
+      nota.data = DateTime.now().toString();
+      print(nota.data);
+      await bancoDados.update(
+          tbAnotacao,
+          nota.toMap(),
+          where: "id = ?",
+          whereArgs: [nota.id]
+      );
+      print("\nnota.toString()");
+      print(nota.toString());
+
+    }
+
+    print ("\n\n\n\n\nfuncionou, carai");
+*/
+
+
+    return anotacoes;
+
+  }
+
+  atualizarAnotacao(Anotacao anotacao) async {
+    var bancoDados = await db;
+
+    return await bancoDados.update(
+      tbAnotacao,
+      anotacao.toMap(),
+      where: "id = ?",
+      whereArgs: [anotacao.id]
+    );
 
   }
 
