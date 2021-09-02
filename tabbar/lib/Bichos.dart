dba9f1ff0a399bc22ed5abb1bd5a9fd5370f8b5f
rvmagno <rvmagno@gmail.com>
2021-09-02 09:20:49 -0300

    ajuste nos nomes das classes; ajuste no background;

---
+++ tabbar/lib/Bichos.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Bichos extends StatefulWidget {
+  const Bichos({Key? key}) : super(key: key);
+
+  @override
+  _BichosState createState() => _BichosState();
+}
+
+class _BichosState extends State<Bichos> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Primeira Pagina"),
+    );
+  }
+}
+
--- tabbar/lib/main.dart
+++ tabbar/lib/main.dart
@@ -1,76 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:tabbar/PrimeiraPagina.dart';
-import 'package:tabbar/SegundaPagina.dart';
-import 'package:tabbar/TerceiraPagina.dart';
+import 'package:tabbar/Home.dart';
+
 
 void main() => runApp(MaterialApp(
   home: Home(),
   debugShowCheckedModeBanner: false,
+  theme: ThemeData(
+    primaryColor: Color(0xff795548),
+    scaffoldBackgroundColor: Color(0xfff5e9b9)
+  ),
 ));
-
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
-
-  late TabController _tabController ;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(
-        length: 3,
-        vsync: this);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _tabController.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Abas"),
-        bottom: TabBar(
-          indicatorColor: Colors.yellow,
-          labelStyle: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-            color: Colors.yellow
-          ),
-          controller: _tabController,
-          tabs: [
-            Tab(
-              text: "Home",
-              icon: Icon(Icons.home_outlined),
-            ),
-            Tab(
-              text: "Email",
-              icon: Icon(Icons.email_outlined),
-            ),
-            Tab(
-              text: "Conta",
-              icon: Icon(Icons.account_circle_outlined),
-            ),
-
-          ],
-        ),
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: [
-          PrimeiraPagina(),
-          SegundaPagina(),
-          TerceiraPagina()
-        ],
-      ),
-    );
-  }
-}
