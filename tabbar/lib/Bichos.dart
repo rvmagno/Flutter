b6c9a9fa5886e615aafba92b808240f3f628a249
Rômulo Magno <romulo.magno@opah.com.br>
2021-09-14 16:51:13 -0300

    incluindo tratamento para executar sons; refactor de codigo;

--- tabbar/lib/Bichos.dart
+++ tabbar/lib/Bichos.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class Bichos extends StatefulWidget {
   const Bichos({Key? key}) : super(key: key);
@@ -8,6 +9,29 @@ class Bichos extends StatefulWidget {
 }
 
 class _BichosState extends State<Bichos> {
+
+
+  final _audiocache = AudioCache(prefix: "assets/audio");
+
+  _executar(String nomeMedia){
+    _audiocache.play("/"+nomeMedia+ ".mp3");
+  }
+
+
+
+  String _loadImg(String img){
+    return "assets/images/" + img + ".png";
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    _audiocache.loadAll([
+      "cao.mp3", "gato.mp3", "leao.mp3", "macaco.mp3", "ovelha.mp3", "vaca.mp3"
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -20,28 +44,40 @@ class _BichosState extends State<Bichos> {
             GestureDetector(
               onTap: (){
                 print("som do cão");
+                _executar("cao");
+
               },
-              child: Image.asset("assets/images/cao.png"),
+              child: Image.asset(_loadImg("cao")),
             ),
             GestureDetector(
-              onTap: (){},
-              child: Image.asset("assets/images/gato.png"),
+              onTap: (){
+                _executar("gato");
+              },
+              child: Image.asset(_loadImg("gato")),
             ),
             GestureDetector(
-              onTap: (){},
-              child: Image.asset("assets/images/leao.png"),
+              onTap: (){
+                _executar("leao");
+              },
+              child: Image.asset(_loadImg("leao")),
             ),
             GestureDetector(
-              onTap: (){},
-              child: Image.asset("assets/images/macaco.png"),
+              onTap: (){
+                _executar("macaco");
+              },
+              child: Image.asset(_loadImg("macaco")),
             ),
             GestureDetector(
-              onTap: (){},
-              child: Image.asset("assets/images/ovelha.png"),
+              onTap: (){
+                _executar("ovelha");
+              },
+              child: Image.asset(_loadImg("ovelha")),
             ),
             GestureDetector(
-              onTap: (){},
-              child: Image.asset("assets/images/vaca.png"),
+              onTap: (){
+                _executar("vaca");
+              },
+              child: Image.asset(_loadImg("vaca") ),
             ),
 
           ],
--- tabbar/lib/Numeros.dart
+++ tabbar/lib/Numeros.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class Numeros extends StatefulWidget {
   const Numeros({Key? key}) : super(key: key);
@@ -8,11 +9,32 @@ class Numeros extends StatefulWidget {
 }
 
 class _NumerosState extends State<Numeros> {
+
+
+  final _audiocache = AudioCache(prefix: "assets/audio");
+  _executar(String nomeMedia){
+    _audiocache.play("/"+nomeMedia+ ".mp3");
+  }
+
+    String _loadImg(String img){
+      return "assets/images/" + img + ".png";
+    }
+
+  @override
+  void initState() {
+    super.initState();
+    _audiocache.loadAll([
+      "1.mp3", "2.mp3", "3.mp3", "4.mp3", "5.mp3", "6.mp3",
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Segunda Pagina, numeros"),
-    );
+    // return Center(
+    //   child: Text("Segunda Pagina, numeros"),
+    // );
+
+
     //
     // return Container(
     //   child:
@@ -48,5 +70,54 @@ class _NumerosState extends State<Numeros> {
     //     ],
     //   )
     // );
+
+
+
+    // Center( child: Text("Primeira Pagina, bichos"), );
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
+      children: [
+        GestureDetector(
+          onTap: (){
+            print("som do um");
+            _executar("1");
+
+          },
+          child: Image.asset(_loadImg("1")),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("2");
+          },
+          child: Image.asset(_loadImg("2")),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("3");
+          },
+          child: Image.asset(_loadImg("3")),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("4");
+          },
+          child: Image.asset(_loadImg("4")),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("5");
+          },
+          child: Image.asset(_loadImg("5")),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("6");
+          },
+          child: Image.asset(_loadImg("6")),
+        ),
+
+      ],
+    );
   }
 }
